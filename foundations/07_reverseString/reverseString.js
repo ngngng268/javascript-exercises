7d13de163a898a7c67f54fa0e4f94b906ec1daff
ngngng268 <thugiang268@gmail.com>
2026-01-12 23:17:39 +0700

    finish 07_REVERSESTRING exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,8 +1,9 @@
 const reverseString = function(string) {
-let splitString = string.split("");
-let reverseArray = splitString.reverse();
-let joinArray = reverseArray.join("");
-return joinArray;
+let newString = "";
+ for (let i = (string.length) - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+return newString;
 };
 
 // Do not edit below this line
