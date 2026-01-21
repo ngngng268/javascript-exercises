32fe77151918b752516e1c12ec73d8124190e6e5
ngngng268 <thugiang268@gmail.com>
2026-01-21 21:49:54 +0700

    function can remove all values

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function(arr, a, b) {
-    return arr.filter((num) => (((num)!=a)&&(num!=b)))
+const removeFromArray = function(arr, a, b, c, d) {
+    return arr.filter((num) => (
+        ((num)!=a)
+        &&(num!=b)
+        &&(num!=c)
+        &&(num!=d)
+    ))
 };
 
 // Do not edit below this line
