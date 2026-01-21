ee5476ff31f747f31ec3686e11dae15ce0089a1d
ngngng268 <thugiang268@gmail.com>
2026-01-21 22:16:11 +0700

    function returns ERROR with negative numbers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
 const sumAll = function(a, b) {
-    return (
+    if (
+        (a<0 || b<0)
+    ) {
+        return "ERROR"
+    }
+    else return (
         ((a+b) * ((Math.abs(a-b)) + 1))/2
     )
 };
