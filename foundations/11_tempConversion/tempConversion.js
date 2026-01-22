38c8e75a7b4fdf2d61f3d4caf5bfb06eaa51f4a6
ngngng268 <thugiang268@gmail.com>
2026-01-22 20:50:27 +0700

    finish 11_TEMPCONVERSION

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return c = ((Math.round(
+  ((f-32)*5/9)*10)) / 10)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(a) {
+   return b = ((Math.round(
+  (a/5*9+32)*10)
+             )/10)
 };
 
 // Do not edit below this line
