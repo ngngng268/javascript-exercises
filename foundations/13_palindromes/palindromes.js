1120eaffdb31f4322e263e49283e6bd0cd8cec01
ngngng268 <thugiang268@gmail.com>
2026-02-03 22:12:35 +0700

    works with single words

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function palindromes(str) {
+  let strCopy = str ;
+  let reverseStr = strCopy.split("").reverse().join("") ;
+  if (str === reverseStr) {return true}
+  else {return false}
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
