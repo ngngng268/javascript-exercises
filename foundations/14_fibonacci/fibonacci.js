b47424220eb6326465fd8fd5b5d1e3150c68c4e4
ngngng268 <thugiang268@gmail.com>
2026-02-03 22:25:01 +0700

    finish 14_fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function fibonacciNo (n) {
+  const f = [];
+  f[0] = 0 ;
+  f[1] = 1 ;
+  function fibonacciTo (a) {
+    for (i=2; i<=a; i++) {
+      f[i] = f[(i-1)] + f[(i-2)]
+    };
+  };
+  if (n >= 2) {
+    fibonacciTo(n);
+    return (f[n]);
+  } else if (n < 0) {
+    return "OOPS"
+  } else {return(f[n]);};
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
