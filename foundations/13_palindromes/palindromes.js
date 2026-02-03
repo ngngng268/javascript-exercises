d4a47df14799dac0b830e5a7a7245afee15d1051
ngngng268 <thugiang268@gmail.com>
2026-02-03 23:04:45 +0700

    works with upper-case letters

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,5 @@
 const palindromes = function palindromes(str) {
-  str = str.replace(/[^\w\']|_/g, "") ;
+  str = str.replace(/[^\w\']|_/g, "").toLowerCase() ;
   let strCopy = str ;
   let reverseStr = strCopy.split().reverse().join("") ;
   if (str === reverseStr) {return true}
