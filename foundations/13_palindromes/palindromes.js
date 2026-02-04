0832b41fbf119cf6af9bcaace99a88bf65fddb7a
ngngng268 <thugiang268@gmail.com>
2026-02-04 21:14:19 +0700

    finish 13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,7 +1,9 @@
 const palindromes = function palindromes(str) {
   str = str.replace(/[^\w\']|_/g, "").toLowerCase() ;
-  let strCopy = str ;
-  let reverseStr = strCopy.split().reverse().join("") ;
+  function reverse (strng) {
+    return strng.split("").reverse().join("")
+  }
+  let reverseStr = reverse(str) ;
   if (str === reverseStr) {return true}
   else {return false}
 };
