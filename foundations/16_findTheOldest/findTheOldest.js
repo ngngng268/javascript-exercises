e1268d26c5a19b7f2c46cd3086f47f29312fa8f2
ngngng268 <thugiang268@gmail.com>
2026-02-04 21:37:27 +0700

    finish 16_findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function findTheOldest (arr) {
+  const currentYear = new Date().getFullYear() ; //get current year
+  arr.map(person => {
+    if (person.yearOfDeath !== undefined) {
+       (person["age"] = (parseInt(person.yearOfDeath) - parseInt(person.yearOfBirth)))
+      } else { (person["age"] = (currentYear - parseInt(person.yearOfBirth)))}
+    }, []) ;
+  arr.sort((a,b) => (a.age - b.age)).reverse();
+  return (arr[0]);  
 };
 
 // Do not edit below this line
