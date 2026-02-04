e3c73af920f8b50baa17280ae7250436ed6f729f
ngngng268 <thugiang268@gmail.com>
2026-02-04 21:19:54 +0700

    finish 15_getTheTitles exercise

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function getTheTitles(booksList) {
+  return booksList.reduce((titlesList, book, index) => {
+    titlesList[index] = (book.title);
+    return titlesList ;
+  }, []);
 };
 
 // Do not edit below this line
